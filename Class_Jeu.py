fa4aeb3a9f0f2c02a9e49b18edf874816479b7b1
BilelGhouaiel <bilelg@icloud.com>
2024-11-18 17:35:27 +0100

    ALIEEEEEEEEEEEEEEEEEEN

--- Class_Jeu.py
+++ Class_Jeu.py
@@ -7,20 +7,30 @@ from random import randint
 class Jeu(fenetre):
 
     def __init__():
-        self.Liste_Personnages = []
+        self.Liste_Personnages = [0,0,0,0,0,0,0,0,0,0,0,0]
         self.Liste_Projectile = []
         self.Temps_Début = gmtime()
         self.Nb_Alien=0
 
     def create_Alien():
-        p=randint(0,10)
-        if p>4:
-            Alien=
+        L=[]
+        for i in range(1,len(self.Liste_Personnage)):
+            Alieni
+            if self.Liste_Personnage[i]==0:
+                L.append(i)
+        indiceL=randint(len(L))
+        indice=L[indiceL]
+        self.Liste_Personnages[indice] = Alien(...)
+
 
 
 
     Visuel()
-    Joueur= Personnage(canvas)
+    Joueur= Vaisseau(canvas)
     Bloc1= bloc()
+    Bloc2= bloc()
+    self.Liste_Personnage[0],self.Liste_Personnage[10],self.Liste_Personnage[11]=[Joueur,3],[Bloc1,3],[Bloc2,3]
+
+
 
     canvas.after(10,create_Alien())
