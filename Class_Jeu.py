6fc7e876d739499c556301480ed80f8dd0e33ae2
BilelGhouaiel <bilelg@icloud.com>
2024-11-21 10:06:23 +0100

    COMMENTAIRES

--- Class_Jeu.py
+++ Class_Jeu.py
@@ -1,3 +1,10 @@
+#Fait par Calisto Del Aguila et Bilel Ghouaiel
+#implémentation de la classe Jeu du space invader
+# #Fait le 18 novembre 2024
+# A améliorer: l'algorithme d'apparition des aliens
+# A ajouter: Gérer les interactions entre les projectils, les blocs et les personnages
+
+
 import tkinter as tk
 import Class_Projectile
 import Main_Space_Invader
@@ -15,7 +22,6 @@ class Jeu(fenetre):
     def create_Alien():
         L=[]
         for i in range(1,len(self.Liste_Personnage)):
-            Alieni
             if self.Liste_Personnage[i]==0:
                 L.append(i)
         indiceL=randint(len(L))
